3c730a2863c318c00b285a895963476a0f041171
Youngwook Kevin Seo <youngseo89@gmail.com>
2024-12-16 10:05:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//Create a string for the user
+const str =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//declaring variable num1 with +
+let num1 = 2 + 8;
+//declaring variable num2 with *
+let num2 = 10 * 4;
+//declaring variable num3 with -
+let num3 = 40 - 1;
+//checking three assigned variables and commenting it out after it
+//console.log(num1, num2, num3)
+//Creating the dialog box that contains the string and all three numbers
+alert(`${str} ${num1}, ${num2}, ${num3}.`);
